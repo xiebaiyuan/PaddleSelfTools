6fbc2d790ababc08cbfcbd15f291c1af361c3c0f
xiebaiyuan <xiebaiyuan@139.com>
2018-12-27 13:52:14 +0800

    optimise conv mem

--- python/modeltools/tools/model_optimise.py
+++ python/modeltools/tools/model_optimise.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 import os
 
 from core import framework_pb2 as framework_pb2
@@ -8,123 +9,158 @@ MEM1_0___ = 'MEM1_OUT'
 MEM1_1___ = MEM1_0___
 MEM1_2___ = 'MEM1_FEED'
 
-
-def replace_argu(argu, input):
-    if 'Convolution10.conv2d.output.1.tmp_0' in argu:
-        del input.arguments[:]
-        input.arguments.extend([MEM1_0___])
-        return
-    if 'Convolution10.conv2d.output.1.tmp_1' in argu:
-        del input.arguments[:]
-        input.arguments.extend([MEM1_1___])
-        return
-    if 'Eltwise1.add.output.1.tmp_0' in argu:
-        del input.arguments[:]
-        input.arguments.extend([MEM1_0___])
-        return
-    if 'tmp_0' in argu:
-        del input.arguments[:]
-        input.arguments.extend([MEM16_0___])
-        return
-    if 'tmp_1' in argu:
-        del input.arguments[:]
-        input.arguments.extend([MEM16_1___])
-        return
-
-    if 'Input1' in argu:
-        del input.arguments[:]
-        input.arguments.extend([MEM1_2___])
-        return
+last_mem = MEM16_0___
 
 
-def replace_var(blocks, name):
-    for block in blocks:
-        for var in block.vars:
-            if var.name == name:
+class Optimiser:
+    next_input = MEM16_1___
+    next_output = MEM16_0___
 
-                if 'Convolution10.conv2d.output.1.tmp_0' in var.name:
-                    var.name = MEM1_0___
-                    break
-                if 'Convolution10.conv2d.output.1.tmp_1' in var.name:
-                    var.name = MEM1_1___
-                    break
-                if 'Eltwise1.add.output.1.tmp_0' in var.name:
-                    var.name = MEM1_0___
-                    break
+    def __init__(self):
+        pass
 
-                if 'tmp_0' in var.name:
-                    var.name = MEM16_0___
-                    break
+    def inner_pack_16_argu(self, argu, input, value):
+        if 'tmp_0' in argu:
+            del input.arguments[:]
+            input.arguments.extend([value])
+            return True
 
-                if 'tmp_1' in var.name:
-                    var.name = MEM16_1___
-                    break
+        if 'tmp_1' in argu:
+            del input.arguments[:]
+            input.arguments.extend([value])
+            return True
+        return False
 
-                if 'Input1' in var.name:
-                    var.name = MEM1_2___
-                    break
+    def replace_argu(self, argu, input, is_conv, is_input):
+        if 'Convolution10.conv2d.output.1.tmp_0' in argu:
+            del input.arguments[:]
+            input.arguments.extend([MEM1_0___])
+            return
+        if 'Convolution10.conv2d.output.1.tmp_1' in argu:
+            del input.arguments[:]
+            input.arguments.extend([MEM1_1___])
+            return
+        if 'Eltwise1.add.output.1.tmp_0' in argu:
+            del input.arguments[:]
+            input.arguments.extend([MEM1_0___])
+            return
 
+        if 'Input1' in argu:
+            del input.arguments[:]
+            input.arguments.extend([MEM1_2___])
+            return
 
-def read_model(model_path):
-    print('read_model.')
-    path_8 = unicode(model_path, 'utf8')
+        if is_conv:
+            # 卷积
+            if is_input:
+                # 输入
+                if self.inner_pack_16_argu(argu, input, self.next_input):
+                    if self.next_input == MEM16_0___:
+                        self.next_input = MEM16_1___
+                    else:
+                        self.next_input = MEM16_0___
+                    return
+                pass
+            else:
+                # 输出
+                if self.inner_pack_16_argu(argu, input, self.next_input):
+                    return
+                pass
 
-    try:
-        with open(path_8, "rb") as f_model:
-            print get_file_size(model_path)
-            desc = framework_pb2.ProgramDesc()
-            desc.ParseFromString(f_model.read())
+        else:
+            # 输出
+            if self.inner_pack_16_argu(argu, input, self.next_input):
+                return
+            pass
 
-            blocks = desc.blocks
-            for block in blocks:
-                # print block
-                ops = block.ops
-                for op in ops:
-                    print '----------op: ---------------'
-                    print op.type
-                    # print op.inputs
-                    # print op.outputs
+    def replace_var(self, blocks, name):
+        for block in blocks:
+            for var in block.vars:
+                if var.name == name:
 
-                    # print op
-                    if 1:
-                        print 'input ----:::'
-                        for input in op.inputs:
-                            # print 'argument:'
-                            # print 'an input :'
-                            for argu in input.arguments:
-                                if 'weights' in argu:
-                                    continue
-                                if 'biases' in argu:
-                                    continue
-                                replace_var(blocks, argu)
-                                # if argu.startswith('Convolution'):
-                                replace_argu(argu, input)
+                    if 'Convolution10.conv2d.output.1.tmp_0' in var.name:
+                        var.name = MEM1_0___
+                        break
+                    if 'Convolution10.conv2d.output.1.tmp_1' in var.name:
+                        var.name = MEM1_1___
+                        break
+                    if 'Eltwise1.add.output.1.tmp_0' in var.name:
+                        var.name = MEM1_0___
+                        break
 
-                        print 'output ----:::'
+                    if 'Input1' in var.name:
+                        var.name = MEM1_2___
+                        break
 
-                        for output in op.outputs:
-                            # print 'argument:'
-                            # print 'an input :'
-                            for argu in output.arguments:
-                                if 'weights' in argu:
-                                    continue
-                                if 'biases' in argu:
-                                    continue
-                                replace_var(blocks, argu)
-                                replace_argu(argu, output)
+                    if 'tmp_0' in var.name:
+                        var.name = MEM16_0___
+                        break
 
-                                # print input
-                        # print input.arguments
+                    if 'tmp_1' in var.name:
+                        var.name = MEM16_1___
+                        break
 
-            print desc
+    def read_model(self, model_path):
+        print('read_model.')
+        path_8 = unicode(model_path, 'utf8')
 
-            f = open("/Users/xiebaiyuan/PaddleProject/GITHUB/PaddleMobileModelTools/python/modeltools/tools/optimised/model",
-                     "wb")
-            f.write(desc.SerializeToString())
-            f.close()
+        try:
+            with open(path_8, "rb") as f_model:
+                print get_file_size(model_path)
+                desc = framework_pb2.ProgramDesc()
+                desc.ParseFromString(f_model.read())
 
-    except IOError:
-        print ": File not found."
+                blocks = desc.blocks
+                for block in blocks:
+                    # print block
+                    ops = block.ops
+                    for op in ops:
+                        print '----------op: ---------------'
+                        print op.type
+                        # print op.inputs
+                        # print op.outputs
+
+                        # print op
+                        if 1:
+                            print 'input ----:::'
+                            for input in op.inputs:
+                                # print 'argument:'
+                                # print 'an input :'
+                                for argu in input.arguments:
+                                    if 'weights' in argu:
+                                        continue
+                                    if 'biases' in argu:
+                                        continue
+                                    self.replace_var(blocks, argu)
+                                    # if argu.startswith('Convolution'):
+                                    self.replace_argu(argu, input, op.type == 'conv2d', 1)
+
+                            print 'output ----:::'
+
+                            for output in op.outputs:
+                                # print 'argument:'
+                                # print 'an input :'
+                                for argu in output.arguments:
+                                    if 'weights' in argu:
+                                        continue
+                                    if 'biases' in argu:
+                                        continue
+                                    self.replace_var(blocks, argu)
+                                    self.replace_argu(argu, output, op.type == 'conv2d', 0)
+
+                                    # print input
+                            # print input.arguments
+
+                print desc
+
+                f = open(
+                    "/Users/xiebaiyuan/PaddleProject/GITHUB/PaddleMobileModelTools/python/modeltools/tools/optimised/model",
+                    "wb")
+                f.write(desc.SerializeToString())
+                f.close()
+
+        except IOError:
+            print ": File not found."
 
 
 def get_file_size(file_path):
@@ -135,4 +171,4 @@ def get_file_size(file_path):
 
 
 path = '/Users/xiebaiyuan/PaddleProject/paddle-mobile/test/models/superresoltion_backuo/model'
-read_model(path)
+Optimiser().read_model(path)
