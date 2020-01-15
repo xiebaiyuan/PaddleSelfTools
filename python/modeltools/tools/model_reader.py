b9301f29a6853d82867958fd75ccdd5020db93a3
xiebaiyuan <xiebaiyuan@139.com>
2020-01-15 17:10:13 +0800

    model reader python3

--- python/modeltools/tools/model_reader.py
+++ python/modeltools/tools/model_reader.py
@@ -1,30 +1,31 @@
-import os
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+__author__ = 'xiebaiyuan'
 
-from core import framework_pb2 as framework_pb2
+import framework_pb2
+import os
 
 
 def read_model(model_path):
     print('read_model.')
-    path_8 = unicode(model_path, 'utf8')
-
     try:
-        with open(path_8, "rb") as f_model:
-            print get_file_size(model_path)
+        with open(model_path, "rb") as f_model:
+            print(get_file_size(model_path))
             desc = framework_pb2.ProgramDesc()
             desc.ParseFromString(f_model.read())
-            print desc
+            print(desc)
             # print desc.blocks
 
     except IOError:
-        print ": File not found."
+        print(": File not found.")
 
 
 def get_file_size(file_path):
-    file_path = unicode(file_path, 'utf8')
     fsize = os.path.getsize(file_path)
     fsize = fsize / float(1024 * 1024)
     return round(fsize, 2)
 
 
-path = '/Users/xiebaiyuan/PaddleProject/GITHUB/PaddleMobileModelTools/python/modeltools/testmodelgen/datas/target/testmodel/__model__'
-read_model(path)
+if __name__ == '__main__':
+    path = 'Your model path .'
+    read_model(path)
