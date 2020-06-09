716c458219321c16c859fcaba83c64da67d3584b
xiebaiyuan <xiebaiyuan@139.com>
2020-06-09 12:26:03 +0800

    remote performance test

--- lite/remote/deploy_remote_caffe_mobilenetv1_performance_gcc.sh
+++ lite/remote/deploy_remote_caffe_mobilenetv1_performance_gcc.sh
@@ -2,8 +2,8 @@
 set -o errexit
 build_dir=build.lite.android.armv7.gcc.opencl
 pwd
-scp Ubuntu_home:/workspace/Paddle-Lite/${build_dir}/inference_lite_lib.android.armv7.opencl/demo/cxx/mobile_light ./
-adb push ./mobile_light /data/local/tmp/opencl/mobile_light
+scp Ubuntu_home:/workspace/Paddle-Lite/${build_dir}/inference_lite_lib.android.armv7.opencl/demo/cxx/mobile_light/mobilenetv1_light_api ./
+adb push ./mobilenetv1_light_api /data/local/tmp/opencl/mobilenetv1_light_api
 model_name="/data/local/tmp/opencl/benchmodels26/caffe2pd_mobilenetv1_opencl_fp32_opt_releasev2.6_b8234efb_20200423.nb"
 cmd="export GLOG_v=0;cd /data/local/tmp/opencl/; export LD_LIBRARY_PATH=.; /data/local/tmp/opencl/mobilenetv1_light_api ${model_name}  1 3 224 224 1000 20 0"
 echo ${cmd}
