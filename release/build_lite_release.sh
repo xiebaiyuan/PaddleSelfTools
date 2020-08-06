1d782dbbc0bbb1f3a0c087f3a793ca148560b092
xiebaiyuan <xiebaiyuan@139.com>
2020-08-06 15:50:53 +0800

    增加lite release脚本

---
+++ release/build_lite_release.sh
@@ -0,0 +1,51 @@
+echo '关闭omp.'
+sed -i 's/LITE_WITH_OPENMP "Enable OpenMP in lite framework" ON/LITE_WITH_OPENMP "Enable OpenMP in lite framework" OFF/' CMakeLists.txt
+cat CMakeLists.txt | grep "Enable OpenMP"
+echo 'arm v8'
+./lite/tools/build_android.sh \
+--arch=armv8 \
+--toolchain=clang \
+--android_stl=c++_static \
+--with_java=ON \
+--with_cv=ON \
+--with_log=OFF \
+--with_extra=ON \
+--with_opencl=OFF
+
+echo 'arm v7'
+./lite/tools/build_android.sh \
+--arch=armv7 \
+--toolchain=clang \
+--android_stl=c++_static \
+--with_java=ON \
+--with_cv=ON \
+--with_log=OFF \
+--with_extra=ON \
+--with_opencl=OFF
+
+
+echo 'opencl v8'
+./lite/tools/build_android.sh \
+--arch=armv8 \
+--toolchain=clang \
+--android_stl=c++_static \
+--with_java=ON \
+--with_cv=ON \
+--with_log=OFF \
+--with_extra=ON \
+--with_opencl=ON
+
+echo 'opencl v7'
+./lite/tools/build_android.sh \
+--arch=armv7 \
+--toolchain=clang \
+--android_stl=c++_static \
+--with_java=ON \
+--with_cv=ON \
+--with_log=OFF \
+--with_extra=ON \
+--with_opencl=ON
+
+echo '打开omp'
+sed -i 's/LITE_WITH_OPENMP "Enable OpenMP in lite framework" OFF/LITE_WITH_OPENMP "Enable OpenMP in lite framework" ON/' CMakeLists.txt
+cat CMakeLists.txt | grep "Enable OpenMP"
