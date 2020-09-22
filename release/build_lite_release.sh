7a83e71e0a7e34826e8b91ffcc371b9921aa11ce
xiebaiyuan <xiebaiyuan@139.com>
2020-09-22 18:26:40 +0800

    optimise build script

--- release/build_lite_release.sh
+++ release/build_lite_release.sh
@@ -1,34 +1,46 @@
 set -ex
 git fetch
 git merge
+
 export NDK_ROOT=/opt/android-ndk-r17c
+# 删除上一次的构建产物
+#rm -rf build.*
+# 删除上一次CMake自动生成的.h文件
+rm ./lite/api/paddle_use_kernels.h
+rm ./lite/api/paddle_use_ops.h
 
 echo "NDK_ROOT: ${NDK_ROOT}"
 echo "ANDROID_NDK: ${ANDROID_NDK}"
 echo '关闭omp.'
 sed -i 's/LITE_WITH_OPENMP "Enable OpenMP in lite framework" ON/LITE_WITH_OPENMP "Enable OpenMP in lite framework" OFF/' CMakeLists.txt
 cat CMakeLists.txt | grep "Enable OpenMP"
+
+echo "修改线程数量"
+sed -i 's/readonly NUM_PROC=${LITE_BUILD_THREADS:-4}/readonly NUM_PROC=30 #${LITE_BUILD_THREADS:-4}/' ./lite/tools/build.sh
+sed -i 's/readonly NUM_PROC=${LITE_BUILD_THREADS:-4}/readonly NUM_PROC=30 #${LITE_BUILD_THREADS:-4}/' ./lite/tools/build_android.sh
+sed -i 's/readonly NUM_PROC=${LITE_BUILD_THREADS:-4}/readonly NUM_PROC=30 #${LITE_BUILD_THREADS:-4}/' ./lite/tools/ci_build.sh
+
 echo 'arm v8'
-./lite/tools/build_android.sh \
---arch=armv8 \
---toolchain=clang \
---android_stl=c++_static \
---with_java=ON \
---with_cv=ON \
---with_log=OFF \
---with_extra=ON \
---with_opencl=OFF
+#./lite/tools/build_android.sh \
+#--arch=armv8 \
+#--toolchain=clang \
+#--android_stl=c++_static \
+#--with_java=ON \
+#--with_cv=ON \
+#--with_log=OFF \
+#--with_extra=ON \
+#--with_opencl=OFF
 
 echo 'arm v7'
-./lite/tools/build_android.sh \
---arch=armv7 \
---toolchain=clang \
---android_stl=c++_static \
---with_java=ON \
---with_cv=ON \
---with_log=OFF \
---with_extra=ON \
---with_opencl=OFF
+#./lite/tools/build_android.sh \
+#--arch=armv7 \
+#--toolchain=clang \
+#--android_stl=c++_static \
+#--with_java=ON \
+#--with_cv=ON \
+#--with_log=OFF \
+#--with_extra=ON \
+#--with_opencl=OFF
 
 
 echo 'opencl v8'
@@ -60,5 +72,8 @@ echo '打开omp'
 sed -i 's/LITE_WITH_OPENMP "Enable OpenMP in lite framework" OFF/LITE_WITH_OPENMP "Enable OpenMP in lite framework" ON/' CMakeLists.txt
 cat CMakeLists.txt | grep "Enable OpenMP"
 
+git checkout ./lite/tools/build.sh
+git checkout ./lite/tools/build_android.sh
+git checkout ./lite/tools/ci_build.sh
 echo '打包发布'
 ./PaddleSelfTools/release/build_lite_release_zip.sh
